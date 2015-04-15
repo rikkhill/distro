1e859a2f85b2a5ccf2ac0f77323df88933764772
Rikk Hill <rikk.hill@net-a-porter.com>
2015-04-15 15:52:27 +0100

    More sophisticated histogram and added jquery

--- js/main.js
+++ js/main.js
@@ -5,50 +5,58 @@
  */
 
 // probability histogram in D3
-var histogram = function(element) {
+var histogram = function(element, x_domain, bins) {
 
+    // Display variables
     var parent_element = element;
-    var bins = 20;
-
     var margin = {
         top: 10,
         right: 30,
         bottom: 30,
         left: 30
     }
-
     var width = element.offsetWidth - margin.left - margin.right;
     var height = element.offsetHeight - margin.top - margin.bottom;
 
-    this.plot = function(data, x_domain) {
+    var bins = bins;
+    var domain = x_domain;
+    var y_scale = d3.scale.linear().domain([0, 1]).range([height, 0]);
+    var x_scale = d3.scale.linear().domain(domain).range([0, width]);
 
-        var x_scale = d3.scale.linear().domain(x_domain).range([0, width]);
-        var y_scale = d3.scale.linear().domain([0, 1]).range([height, 0]);
-        var binned_data = d3.layout.histogram()
-            .bins(x_scale.ticks(bins))
-            .frequency(false)(data);
+    var bin_data = d3.layout.histogram()
+        .bins(x_scale.ticks(bins))
+        .frequency(false);
 
-        var container = d3.select(parent_element).append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
+    // svg container, transformed for margins
+    d3.select(parent_element).select("svg").remove();
+    var container = d3.select(parent_element).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        var bar = container.selectAll(".bar").data(binned_data);
-        bar.exit().remove();
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x_axis = d3.svg.axis().scale(x_scale).orient("bottom");
+
+    container.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(x_axis);
+
+    var plot = function(data) {
+
+        container.selectAll(".bar").remove();
+
+        var hist_data = bin_data(data);
+        var bar = container.selectAll(".bar").data(hist_data);
+
         bar.enter().append("g")
             .attr("class", "bar")
             .attr("transform", function(d) { return "translate(" + x_scale(d.x) + "," + y_scale(d.y) + ")";  });
 
         bar.append("rect")
             .attr("x", 1)
-            .attr("width", x_scale(binned_data[0].dx) - 1)
+            .attr("width", x_scale(1) - 1)
             .attr("height", function(d) { return height - y_scale(d.y) });
-
-        x_axis = d3.svg.axis().scale(x_scale).orient("bottom");
-        container.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(x_axis);
     }
 
     return {
